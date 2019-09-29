450bd1760c6315e492252dce416bbff92f6a50e5
TheDerpGamer <garret2kool@gmail.com>
2019-09-29 13:44:21 -0700

    Create GUISlot.java

---
+++ src/thederpgamer/derpcore/gui/GUISlot.java
@@ -0,0 +1,34 @@
+package thederpgamer.derpcore.gui;
+
+import java.lang.reflect.Method;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Item;
+
+import thederpgamer.derpcore.blocks.CustomBlock;
+import thederpgamer.derpcore.blocks.CustomItem;
+
+public class GUISlot {
+	public Item item;
+	public Block block;
+	public CustomItem citem;
+	public CustomBlock cblock;
+	public String displayName;
+	public int slot;
+	public Method clickFunction;
+
+	public GUISlot(Item guiItem, String displayName, int slot, Method onClick) {
+		
+	}
+	
+	public GUISlot(Block guiBlock, String displayName, int slot, Method onClick) {
+		
+	}
+	
+	public GUISlot(CustomItem customGUIItem, String displayName, int slot, Method onClick) {
+		
+	}
+	
+	public GUISlot(CustomBlock customGuiBlock, String displayName, int slot, Method onClick) {
+		
+	}
+}
