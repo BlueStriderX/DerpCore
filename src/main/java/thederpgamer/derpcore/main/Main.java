98840cdc2fc401e5d7d1e3483ea61851c2a4670b
TheDerpGamer <garret2kool@gmail.com>
2019-09-29 21:34:38 -0700

    Update Main.java

--- src/main/java/thederpgamer/derpcore/main/Main.java
+++ src/main/java/thederpgamer/derpcore/main/Main.java
@@ -1,6 +1,9 @@
 package main.java.thederpgamer.derpcore.main;
 
+import java.util.ArrayList;
 import org.bukkit.plugin.java.JavaPlugin;
+import main.java.thederpgamer.derpcore.blocks.CustomBlockHandler;
+import main.java.thederpgamer.derpcore.blocks.CustomBlock;
 
 public class Main extends JavaPlugin {
 	
@@ -9,7 +12,8 @@ public class Main extends JavaPlugin {
   public String VERSION = "0.1";
   public String AUTHOR = "TheDerpGamerX";
   public String pluginFile = NAME + "v" + VERSION;
-
+  
+  public static ArrayList<CustomBlock> customBlocks = new ArrayList<CustomBlock>();
   
   @Override
   public void onEnable() {
@@ -17,7 +21,7 @@ public class Main extends JavaPlugin {
     System.out.println("Loading " + pluginFile + "...");
     
     //Events
-    //getServer().getPluginManager().registerEvents(new <EventClass>(), this);
+    getServer().getPluginManager().registerEvents(new CustomBlockHandler(), this);
     
     //Commands
     //getCommand("<command>").setExecutor(new <CommandClass>());
@@ -25,5 +29,9 @@ public class Main extends JavaPlugin {
   
   public static Main getPlugin() {
 	  return plugin;
+  }
+  
+  public static void writeCustomBlock(CustomBlock customBlock) {
+	  
   }
 }
