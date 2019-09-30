1fccec4133077b7243cac6136a28d8b5a9e4fe71
TheDerpGamer <garret2kool@gmail.com>
2019-09-29 21:34:15 -0700

    Update CustomBlock.java

--- src/main/java/thederpgamer/derpcore/blocks/CustomBlock.java
+++ src/main/java/thederpgamer/derpcore/blocks/CustomBlock.java
@@ -1,5 +1,17 @@
 package main.java.thederpgamer.derpcore.blocks;
 
+import main.java.thederpgamer.derpcore.main.Main;
+import net.minecraft.server.v1_14_R1.Block;
+import net.minecraft.server.v1_14_R1.Blocks;
+
 public class CustomBlock {
+	
+	public Block baseblock = Blocks.SPAWNER;
+	public String displayName;
 	public int modelData;
+	
+	public void register() {
+		Main.customBlocks.add(this);
+		Main.writeCustomBlock(this);
+	}
 }
