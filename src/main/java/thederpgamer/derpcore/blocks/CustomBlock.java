ecccaebb23d0b67712ac93e60e4874c7a0f82103
TheDerpGamer <garret2kool@gmail.com>
2019-09-30 11:57:16 -0700

    Update CustomBlock.java

--- src/main/java/thederpgamer/derpcore/blocks/CustomBlock.java
+++ src/main/java/thederpgamer/derpcore/blocks/CustomBlock.java
@@ -1,5 +1,10 @@
 package main.java.thederpgamer.derpcore.blocks;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
 import main.java.thederpgamer.derpcore.main.Main;
 import net.minecraft.server.v1_14_R1.Block;
 import net.minecraft.server.v1_14_R1.Blocks;
@@ -7,11 +12,20 @@ import net.minecraft.server.v1_14_R1.Blocks;
 public class CustomBlock {
 	
 	public Block baseblock = Blocks.SPAWNER;
+	public String name;
 	public String displayName;
 	public int modelData;
 	
 	public void register() {
 		Main.customBlocks.add(this);
-		Main.writeCustomBlock(this);
+		try {
+			Main.writeCustomBlock(this);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
