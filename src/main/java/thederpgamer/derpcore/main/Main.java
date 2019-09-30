77420d7165b6b7df378e47b6decb0d7d02d252a6
TheDerpGamer <garret2kool@gmail.com>
2019-09-29 22:07:45 -0700

    Update Main.java

--- src/main/java/thederpgamer/derpcore/main/Main.java
+++ src/main/java/thederpgamer/derpcore/main/Main.java
@@ -1,7 +1,11 @@
 package main.java.thederpgamer.derpcore.main;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import main.java.thederpgamer.derpcore.blocks.CustomBlockHandler;
 import main.java.thederpgamer.derpcore.blocks.CustomBlock;
 
@@ -31,7 +35,36 @@ public class Main extends JavaPlugin {
 	  return plugin;
   }
   
+  @SuppressWarnings("unchecked")
   public static void writeCustomBlock(CustomBlock customBlock) {
+	  JSONObject block = new JSONObject();
+	  JSONObject head = new JSONObject();
 	  
+	  JSONArray rotation = new JSONArray();
+	  rotation.add(-30);
+	  rotation.add(0);
+	  rotation.add(0);
+	  
+	  JSONArray translation = new JSONArray();
+	  translation.add(0);
+	  translation.add(-30.75);
+	  translation.add(-7.25);
+	  
+	  JSONArray scale = new JSONArray();
+	  scale.add(3.0125);
+	  scale.add(3.0125);
+	  scale.add(3.0125);
+	  
+	  head.put("rotation", rotation);
+	  head.put("translation", translation);
+	  head.put("scale", scale);
+	  block.put("display", head);
+	  
+	  
+	  try (FileWriter file = new FileWriter("" + blockResourcePath + ".json")) {
+		  file.write(block.toJSONString());
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
   }
 }
